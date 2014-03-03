e69f9a52d8956b96eac794746ad11a448d73c43f
Vytautas Jakutis <vytautas@jakut.is>
2014-03-03 17:34:23 +0000

    test: flatten: add more behaviors

--- test/flatten.js
+++ test/flatten.js
@@ -6,5 +6,14 @@ describe('flatten', function(){
     it('should return same flat array as provided', function(){
         var array = [1, 2, 3];
         assert.deepEqual(array, _.flatten(array));
-    })
+    });
+    it('should flatten 2 levels deep', function(){
+        assert.deepEqual([1, 2, 3], _.flatten([1, [2], 3]));
+    });
+    it('should flatten 0 levels deep', function(){
+        assert.deepEqual([1, 2, 3], _.flatten(1, 2, 3));
+    });
+    it('should work with multiple arguments', function(){
+        assert.deepEqual([1, 2, 3], _.flatten(1, [2], 3));
+    });
 })
